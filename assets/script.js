b2a74803bb52a69723de41fc2bb74a257543af55
TheLegendary1248 <1248alex@gmail.com>
2022-02-02 20:08:00 -0500

    Some cleanup cuz i got bored

--- assets/script.js
+++ assets/script.js
@@ -2,37 +2,16 @@
 // First, tell us your name
 let yourName = "Alejandro Martinez the Improviser" // HINT: Replace this with your own name!
 
-//Generic cookie count changing function
-function t(target)
-{
-    var element = target.target //avoids confusion caused by myself
-    var cookieIndex = parseInt(element.parentNode.getAttribute('class').split("")[0]);
-    if(element.getAttribute('class') == "add") cookieArr[cookieIndex] += 1
-    else cookieArr[cookieIndex] -= 1
-    if(cookieArr[cookieIndex] < 0) cookieArr[cookieIndex] = 0
-    //Update given cookie
-    cookieCounts[cookieIndex].querySelectorAll("td")[1].textContent = cookieArr[cookieIndex]
-    console.log(cookieCounts[cookieIndex].childNodes)
-    //Get total cookies
-    var totalCt = 0
-    for (let index = 0; index < cookieArr.length; index++) totalCt += cookieArr[index]
-    //Update total count
-    tot.textContent = totalCt
-    console.log("Cookie count changed to " + cookieArr.toString())
-}
 //Total count element as variable
 const tot = document.querySelector("#qty-total")
+//Gets each cookie count the hard way as an array
+const cookieCounts = document.querySelector(".summary").querySelector("tbody").querySelectorAll("tr")
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 //how about no?
 let cookieArr = [0,0,0]
-
-// selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// Code to update name display
-credit.textContent = `Created by ${yourName}`
 
 const buttons = document.querySelectorAll(".button-container")
 //Of course im using the function we didn't go over
@@ -50,6 +29,25 @@ for (let index = 0; index < buttons.length; index++) {
     var C = element.getAttribute('class')
     element.setAttribute('class',index.toString() + " " + C)
 }
-//Round of Pain #2
-const cookieCounts = document.querySelector(".summary").querySelector("tbody").querySelectorAll("tr")
-//Wow only 54 lines?
+//Generic cookie count changing function
+function t(target)
+{
+    var element = target.target //avoids confusion caused by myself
+    var cookieIndex = parseInt(element.parentNode.getAttribute('class').split("")[0]);
+    if(element.getAttribute('class') == "add") cookieArr[cookieIndex] += 1
+    else cookieArr[cookieIndex] -= 1
+    if(cookieArr[cookieIndex] < 0) cookieArr[cookieIndex] = 0
+    //Update given cookie
+    cookieCounts[cookieIndex].querySelectorAll("td")[1].textContent = cookieArr[cookieIndex]
+    //Get total cookies
+    var totalCt = 0
+    for (let index = 0; index < cookieArr.length; index++) totalCt += cookieArr[index]
+    //Update total count
+    tot.textContent = totalCt
+    console.log("Cookie count changed to " + cookieArr.toString())
+}
+// selecting the element with an id of credit
+const credit = document.querySelector('#credit')
+// Code to update name display
+credit.textContent = `Created by ${yourName}`
+//Wow only 53 lines?
